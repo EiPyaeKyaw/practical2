757aa07a168d26c14ede545fafd4277e122a0ecd
EiPyaeKyaw <eipyae.23@ichat.sp.edu.sg>
2024-11-17 12:15:22 +0800

    Bmi calculation

---
+++ bmi.py
@@ -0,0 +1,13 @@
+def calculate_bmi(height, weight):
+    print("Height = "+ str(height))
+    print("Weight = "+ str(weight))
+    bmi = weight/(height*height)
+    print("Result is "+ str(bmi))
+    if bmi<18.5:
+        print("Under Weight")
+    elif 18.5<=bmi<=25.0:
+        print("Normal Weight")
+    else:
+        print("Over Weight")
+
+calculate_bmi(height=1.73, weight=57)
